27452f6ae22473caf100496748195858a492d947
BenBrincat <benjaminbrincat@gmail.com>
2017-07-13 11:20:22 +0100

    'new'

---
+++ about.php
@@ -0,0 +1,46 @@
+<?php
+include('header.php');
+?>
+<div class="about">
+	<section class="page-title">
+		<div class="container-fluid">
+			<div class="row">
+				<h1>About Timecare Diaries</h1>
+			</div>
+		</div>	
+	</section>
+
+	<section class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="content-full-width">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6 col-md-offset-3">
+									<p class="para text-center">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor, risus et efficitur molestie, dolor mi suscipit sapien, et lobortis ante eros quis lorem. In eget pretium ligula, nec dictum ipsum. In vitae vestibulum dolor, nec luctus nibh. Sed viverra egestas ex, et ultrices purus placerat vel. Nunc eleifend tincidunt mauris, sit amet mollis lacus auctor vel. Quisque suscipit leo risus, eu lobortis nibh scelerisque in. Mauris id porta leo. Mauris at ante sagittis, tristique justo vitae, vestibulum enim.
+									</p>
+									<p class="para text-center">
+										Aenean egestas sapien vitae quam tristique vehicula. Cras ornare cursus venenatis. Morbi mollis mollis risus ac mollis. Donec tempus dui nunc, non eleifend eros euismod in. Suspendisse potenti. In in libero lobortis, tristique enim ac, efficitur augue. Pellentesque viverra vestibulum est, ut venenatis nisl mattis sit amet. Duis feugiat vulputate laoreet. Donec gravida, felis id molestie ullamcorper, massa ante scelerisque lorem, eget suscipit risus diam interdum nulla. Nulla blandit diam orci, vel vehicula quam mollis ut. Nunc vitae eros at ex ornare rhoncus sit amet sed orci.
+									</p>
+								</div>
+								<div class="col-md-6 col-md-offset-3">
+									<h2>Our brands and suppliers</h2>
+									<p class="para text-center">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor, risus et efficitur molestie, dolor mi suscipit sapien, et lobortis ante eros quis lorem. In eget pretium ligula, nec dictum ipsum. In vitae vestibulum dolor, nec luctus nibh. Sed viverra egestas ex, et ultrices purus placerat vel. Nunc eleifend tincidunt mauris, sit amet mollis lacus auctor vel. Quisque suscipit leo risus, eu lobortis nibh scelerisque in. Mauris id porta leo. Mauris at ante sagittis, tristique justo vitae, vestibulum enim.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+
+<?php
+include('footer.php');
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include('header.php');
+?>
+<h1>Test</h1>
+<h2>Testing Header</h2>
+<?php
+include('footer.php');
+?>
